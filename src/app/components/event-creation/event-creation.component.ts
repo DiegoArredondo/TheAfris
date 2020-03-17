19db5744d307f7528ffbcc4aec68279833c35771
cristinablanco99 <crisb995@gmail.com>
2020-03-17 00:49:32 -0700

    vista crear evento
    vista crear evento

--- src/app/components/event-creation/event-creation.component.ts
+++ src/app/components/event-creation/event-creation.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventCreationComponent implements OnInit {
 
+
+  mensaje="pinche coronavirus"
+
   constructor() { }
 
   ngOnInit(): void {
