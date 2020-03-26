cc02aa833bfd99f129d6163d9b71ab59ead5ec6a
cristinablanco99 <crisb995@gmail.com>
2020-03-25 21:05:55 -0700

    pantallas listaEventos y listaPublicaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { EventCreationComponent } from './components/event-creation/event-creati
 import { EventEditComponent } from './components/event-edit/event-edit.component';
 import { PostCreationComponent } from './components/post-creation/post-creation.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
-
+import { ListaPublicacionesComponent } from './components/lista-publicaciones/lista-publicaciones.component';
+import {ListaEventosComponent} from './components/lista-eventos/lista-eventos.component';
 
 const routes: Routes = [
   {path:"crearEvento", component: EventCreationComponent} as Route,
   {path:"editarEvento", component: EventEditComponent} as Route,
   {path:"crearPublicacion", component: PostCreationComponent} as Route,
   {path:"editarPublicacion", component: PostEditComponent} as Route,
+  {path:"listaPublicaciones", component: ListaPublicacionesComponent} as Route,
+  {path:"listaEventos", component: ListaEventosComponent} as Route,
 ];
 
 @NgModule({
